432c31ac4494786a60a6d0e1fb907f33dd126ce8
Alex Goodman <alexthegoodman@gmail.com>
2025-01-09 08:03:01 -0500

    small reminder description

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -10,8 +10,12 @@ export default function Dashboard() {
 
   return (
     <div>
-      <h1>Dashboard</h1>
-      {data?.email}
+      <h1>Welcome, {data?.email}</h1>
+      <p>
+        You are almost ready to use Stunts! Stunts is your pathway to rapid
+        video creation using your own content. With Stunts generative keyframe
+        animations, you can save hours on manual animating.
+      </p>
       {data?.subscriptionStatus === "ACTIVE" ||
       data?.subscriptionStatus === "TRIALING" ? (
         <p>Download to get started</p>
