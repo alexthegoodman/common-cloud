8c4117bc1815f6681bb3b7338ca195ddb03e52ec
Alex Goodman <alexthegoodman@gmail.com>
2025-01-18 22:13:30 -0500

    tweak metadata

--- app/stunts/layout.tsx
+++ app/stunts/layout.tsx
@@ -1,7 +1,11 @@
-"use client";
-
 import Footer from "@/components/Footer";
+import { Metadata } from "next";
 import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Common Cloud | Stunts",
+  description: "Video and motion graphics editor centered on UX and AI",
+};
 
 export default function StuntsLayout({ children = null }) {
   return (
