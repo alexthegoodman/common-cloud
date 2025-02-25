d36f1934b669dbeb146eb471ab9a7ba9db4689d6
Alex Goodman <alexthegoodman@gmail.com>
2025-02-24 22:37:51 -0500

    add flow and brandkit endpoints

---
+++ app/api/flows/create/route.ts
@@ -0,0 +1,48 @@
+import { NextResponse } from "next/server";
+import { verifyJWT } from "@/lib/jwt";
+import prisma from "@/lib/prisma";
+
+export async function POST(req: Request) {
+  try {
+    const token = req.headers.get("Authorization")?.split(" ")[1];
+
+    if (!token) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const decoded = verifyJWT(token) as { userId: string; email: string };
+
+    const user = await prisma.user.findUnique({
+      where: { id: decoded.userId },
+      include: {
+        plan: true,
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    const { prompt, brandKitId, emptyContent, emptyQuestions } =
+      await req.json();
+
+    const newFlow = await prisma.flow.create({
+      data: {
+        ownerId: user.id,
+        brandKitId,
+        prompt,
+        content: emptyContent,
+        questions: emptyQuestions,
+      },
+    });
+
+    return NextResponse.json({
+      newFlow,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
