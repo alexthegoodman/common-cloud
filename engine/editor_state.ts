a2461124d81934525bff5a8a6ae42d84d45426c0
Alex Goodman <alexthegoodman@gmail.com>
2025-03-03 19:52:27 -0500

    set sequence duration fix

--- engine/editor_state.ts
+++ engine/editor_state.ts
@@ -1379,6 +1379,14 @@ export default class EditorState {
 
     saved_state.sequences.forEach((s) => {
       if (s.id == selected_sequence_id) {
+        if (!s.durationMs) {
+          return;
+        }
+
+        if (s.durationMs < sourceDurationMs) {
+          s.durationMs = sourceDurationMs;
+        }
+
         s.activeVideoItems.push(savable_text);
 
         if (this.supportsMotionPaths && s.polygonMotionPaths) {
