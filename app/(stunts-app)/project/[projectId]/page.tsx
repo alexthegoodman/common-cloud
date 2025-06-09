86fd8e6b03a8613838301bf07092e68ee268324c
Alex Woodman <alexthegoodman@gmail.com>
2025-06-09 01:50:39 -0400

    hub responsiveness and onboarding alert

--- app/(stunts-app)/project/[projectId]/page.tsx
+++ app/(stunts-app)/project/[projectId]/page.tsx
@@ -5,7 +5,7 @@ import ErrorBoundary from "@/components/stunts-app/ErrorBoundary";
 import React, { useState } from "react";
 import { useParams, useRouter } from "next/navigation";
 import { CreateIcon } from "@/components/stunts-app/icon";
-import { Check, Plus } from "@phosphor-icons/react";
+import { Check, Plus, X } from "@phosphor-icons/react";
 import { BrandKitList } from "@/components/stunts-app/BrandKitList";
 import { FlowSteps } from "@/components/stunts-app/FlowSteps";
 import { createFlow } from "@/fetchers/flows";
@@ -16,6 +16,11 @@ export default function Project() {
   const { projectId } = useParams();
   const router = useRouter();
   const [authToken] = useLocalStorage<AuthToken | null>("auth-token", null);
+  // Store hub alert visible state in local storage
+  const [hubAlertVisible, setHubAlertVisible] = useLocalStorage<boolean>(
+    "hub-alert-visible",
+    true
+  );
 
   const [loading, setLoading] = useState<boolean>(false);
   const [prompt, setPrompt] = useState<string>("");
@@ -38,12 +43,38 @@ export default function Project() {
       <ErrorBoundary>
         {/* Error Boundary */}
         <ClientOnly>
-          <div className="flex flex-col justify-center items-center mx-auto">
+          <div className="flex flex-col justify-center items-center mx-auto w-[calc(100vw-100px)] md:w-full">
+            {/* Alert explaining hub */}
+            {hubAlertVisible && (
+              <section className="bg-yellow-100 text-yellow-800 p-4 rounded-lg mb-6 w-full md:w-[600px]">
+                <div className="flex justify-between items-center mb-2">
+                  <h2 className="text-lg font-semibold">Welcome to the Hub!</h2>
+
+                  <button
+                    className="text-gray-500 hover:text-gray-700"
+                    onClick={() => setHubAlertVisible(false)}
+                  >
+                    <X size={20} />
+                  </button>
+                </div>
+                <div className="flex items-center">
+                  <Check className="mr-2" />
+                  <span className="text-sm">
+                    Welcome to the Hub! Here you can create and manage your
+                    flows, which are the quick and easy way to generate
+                    marketing and sales materials that are tailored and
+                    personalized. Alternatively, head straight to the area of
+                    your choice using the navigation on the left.
+                  </span>
+                </div>
+              </section>
+            )}
+
             <h1 className="text-3xl text-center mb-12">Welcome to Hub</h1>
             <FlowSteps step={1} />
-            <div className="flex flex-col">
+            <div className="flex flex-col p-1 md:p-0 w-full">
               <BrandKitList />
-              <div className="flex flex-col justify-center items-center mx-auto w-[600px] rounded-[15px] shadow-[0_0_15px_4px_rgba(0,0,0,0.16)]">
+              <div className="flex flex-col justify-center items-center mx-auto w-full md:w-[600px] rounded-[15px] shadow-[0_0_15px_4px_rgba(0,0,0,0.16)]">
                 <textarea
                   className="w-full p-4 rounded-[15px] rounded-b-none"
                   rows={4}
