ec791467f01c7f88edc71ab7d441a9e50ce6070c
Alex Woodman <alexthegoodman@gmail.com>
2025-06-30 01:06:03 -0400

    show upgrade button for non subscribers

--- app/(stunts-app)/projects/page.tsx
+++ app/(stunts-app)/projects/page.tsx
@@ -3,12 +3,16 @@
 import { ClientOnly } from "@/components/ClientOnly";
 import ErrorBoundary from "@/components/stunts-app/ErrorBoundary";
 import { ProjectsList } from "@/components/stunts-app/ProjectsList";
+import { UpgradeButton } from "@/components/stunts-app/UpgradeButton";
+import useCurrentUser from "@/hooks/useCurrentUser";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 
 export default function Projects() {
   const { t } = useTranslation("common");
+
+  const { data: user } = useCurrentUser();
 
   const router = useRouter();
 
@@ -19,8 +23,13 @@ export default function Projects() {
         {/* Error Boundary */}
         <ClientOnly>
           <div className="container mx-auto py-4">
-            <div className="flex flex-row gap-2 justify-between w-full">
+            <div className="flex flex-row gap-2 mb-2 justify-between w-full">
               <h1 className="text-lg">{t("Projects")}</h1>
+
+              {user?.subscriptionStatus === "INACTIVE" && (
+                <UpgradeButton onClick={() => router.push("/upgrade")} />
+              )}
+
               <button
                 onClick={() => router.push("/create-project")}
                 className="group relative w-lg flex justify-center py-2 px-4 border border-transparent
